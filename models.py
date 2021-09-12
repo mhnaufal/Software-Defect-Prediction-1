99fce7de5b7b5529e54a60ea349c76b2bb73fe07
mhnaufal <aal.pirates16@gmail.com>
2021-09-12 12:31:11 +0700

    Preparation commit

--- models.py
+++ models.py
@@ -15,26 +15,33 @@ def CNN(X, X_train, X_validation, y_train, y_validation):
     img_cols = len(X.columns)
 
     X_train_f = X_train_matrix.reshape(X_train_matrix.shape[0], img_rows, img_cols, 1)
-    X_validation_f = X_validation_matrix.reshape(X_validation_matrix.shape[0], img_rows, img_cols, 1)
+    X_validation_f = X_validation_matrix.reshape(
+        X_validation_matrix.shape[0], img_rows, img_cols, 1
+    )
 
     input_shape = (img_rows, img_cols, 1)
 
-
     model = Sequential()
-    model.add(Conv2D(64, kernel_size=1, activation='relu', input_shape=input_shape))
-    model.add(Conv2D(32, kernel_size=1, activation='relu'))
-    model.add(Conv2D(16, kernel_size=1, activation='relu'))
-    model.add(Conv2D(16, kernel_size=1, activation='relu'))
+    model.add(Conv2D(64, kernel_size=1, activation="relu", input_shape=input_shape))
+    model.add(Conv2D(32, kernel_size=1, activation="relu"))
+    model.add(Conv2D(16, kernel_size=1, activation="relu"))
+    model.add(Conv2D(16, kernel_size=1, activation="relu"))
     model.add(Flatten())
-    model.add(Dense(8, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(8, activation="relu"))
+    model.add(Dense(1, activation="sigmoid"))
 
-    model.compile(loss='binary_crossentropy',
-                    optimizer='adam',
-                    metrics=['accuracy'])
+    model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-    model.fit(X_train_f, y_train_matrix, epochs=100, batch_size=35, validation_data=(X_validation_f, y_validation_matrix), verbose=0)
+    model.fit(
+        X_train_f,
+        y_train_matrix,
+        epochs=100,
+        batch_size=35,
+        validation_data=(X_validation_f, y_validation_matrix),
+        verbose=0,
+    )
     return model
+
 
 # random forest classifer
 def random_forest(X_train, y_train):
@@ -42,10 +49,11 @@ def random_forest(X_train, y_train):
     clf.fit(X_train, y_train)
     return clf
 
+
 # support vector machine classifier
 def SVM(X_train, y_train):
     scaler = MinMaxScaler(feature_range=(-1, 1)).fit(X_train)
     X_train = scaler.transform(X_train)
-    clf = SVC(kernel='rbf', degree=3, gamma='auto')
+    clf = SVC(kernel="rbf", degree=3, gamma="auto")
     clf.fit(X_train, y_train)
     return clf
